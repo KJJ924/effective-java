5c89cb4bc9bbb1ce1014d972c71997976f582a5d
jaejoon <dkansk924@naver.com>
2021-02-11 23:35:56 +0900

    ITEM 16코드

---
+++ src/main/java/com/jaejoon/demo/item16/User.java
@@ -0,0 +1,44 @@
+package com.jaejoon.demo.item16;
+
+public class User { //public class 일때.
+
+    private String name; //해당 클래스의 필드의 맴버를 private 으로 감추자.
+    private int age;
+    private String phone;
+
+
+    public String getName() { // getter 메서드를 통해 데이터를 주자.
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getPhone() {
+        //기존 저장형식이 01012345678 인 경우
+        // 클라이언트에게 넘겨줄때는 010-0000-0000으로 넘겨줄수있다.
+        return this.phone.replaceFirst("(^[0-9]{3})([0-9]{3,4})([0-9]{4})$", "$1-$2-$3");
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // setter 를 쓰게되면 불변객체로 보장을 해주지못한다는데..?
+    // 하지만 제공하지 않아도 얼마든지  클라이언트 측에서 리플렉션을 이용하여 얼마든지 조작이가능하긴한데?
+    // 해당 주제에서는 벗어나니 일단지나가자.
+    public void setAge(int age) {
+        if(age<0) {
+            throw new IllegalArgumentException("음수값을 가질 수 없습니다"); //해당코드 처럼 불변식을 보장해줄수 있다
+        }
+        this.age = age;
+    }
+
+    public void setPhone(String phone) {
+        if(!phone.matches("[0-9]{11}")){
+            throw new IllegalArgumentException("11자리수를 맞춰주세요");
+        }
+        this.phone = phone;
+    }
+}
---
+++ src/test/java/com/jaejoon/demo/item16/Item16Test.java
@@ -0,0 +1,36 @@
+package com.jaejoon.demo.item16;
+
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.ThrowableAssert;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class Item16Test {
+
+    @Test
+    void userExceptionTest(){
+        User user = new User();
+
+        assertThatThrownBy(() -> user.setAge(-10))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("음수값을 가질 수 없습니다");
+    }
+
+    @Test
+    void userPhone(){
+        User user = new User();
+        assertThatThrownBy(()->user.setPhone("010"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("11자리수를 맞춰주세요");
+    }
+
+    @Test
+    void userGetPhone(){
+        User user = new User();
+        user.setPhone("01000000000");
+
+        assertThat(user.getPhone()).isEqualTo("010-0000-0000");
+    }
+}
