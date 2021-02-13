33f2ac5ceee70054f99120421d3c983ab76aa5d7
jaejoon <dkansk924@naver.com>
2021-02-13 23:50:52 +0900

    [add] item17 추가

---
+++ src/main/java/com/jaejoon/demo/item17/item17.java
@@ -0,0 +1,12 @@
+package com.jaejoon.demo.item17;
+
+public class item17 {
+    public static void main(String[] args) {
+        String count = "";
+        for (int i = 0; i <10; i++) {
+            count +=String.valueOf(i);
+            System.out.println(count);
+        }
+    }
+}
+
